376ea033c874ce9ab4264090abea360b60abc505
Ayan209e <ayan209e@gmail.com>
2023-10-17 20:07:36 +0530

    Complete Website

---
+++ index.js
@@ -0,0 +1,117 @@
+// Balance
+const balance = document.getElementById("balance");
+
+// Income
+const money_plus = document.getElementById("money-plus");
+  
+// Expense
+const money_minus = document.getElementById("money-minus");
+  
+// Other Element Variables
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
+// Local Storage
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions')); 
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+// Adding EventListner
+form.addEventListener('submit',addTransaction);
+
+//Add Transaction
+function addTransaction(e){
+  e.preventDefault();
+  if(text.value.trim() === ''){
+    alert('Please Enter Transaction Name')
+  }
+  if(amount.value.trim() === ''){
+    alert('Please Enter Amount')
+  }
+  else{
+    const transaction = {
+      id:generateID(),
+      text:text.value,
+      amount:+amount.value
+    }
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+    text.value='';
+    amount.value='';
+  }
+}
+
+//Generate Random ID
+function generateID(){
+  return Math.floor(Math.random()*1000000000);
+}
+
+//Add Trasactions to DOM list
+function addTransactionDOM(transaction) {
+  //GET sign
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const item = document.createElement("li");
+
+  //Add Class Based on Value
+  item.classList.add(
+    transaction.amount < 0 ? "minus" : "plus"
+  );
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}$${Math.abs(
+    transaction.amount
+  )}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+    `;
+  list.appendChild(item);
+}
+
+//Update the Balance, Income & Expense
+function updateValues() {
+  const amounts = transactions.map(
+    (transaction) => transaction.amount
+  );
+  const total = amounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense =
+    (amounts
+      .filter((item) => item < 0)
+      .reduce((acc, item) => (acc += item), 0) *
+    -1).toFixed(2);
+
+    balance.innerText=`$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
+//Remove Transaction by ID
+function removeTransaction(id){
+  transactions = transactions.filter(transaction => transaction.id !== id);
+  updateLocalStorage();
+  Init();
+}
+
+// Updating Local Storage Transaction
+function updateLocalStorage(){
+  localStorage.setItem('transactions',JSON.stringify(transactions));
+}
+
+//Init App
+function Init() {
+  list.innerHTML = "";
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+Init();
+
